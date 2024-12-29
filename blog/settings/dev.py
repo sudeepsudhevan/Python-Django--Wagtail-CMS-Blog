7a2347dbbcad106f0935ac29bb69119c66e1d698
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-12-29 12:59:01 +0530

    first commit

---
+++ blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!5t@xe*=2ojp^n@1o(5ntyq#7z%%xtaj$ot5qvb4j39v)0jg_%"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
