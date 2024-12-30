aae8922bda5f6daf272e4be442e8badd7eb51674
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-12-30 11:21:42 +0530

    Custom image Handling

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -26,6 +26,8 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     "home",
     "search",
+    "images",
+
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
@@ -178,3 +180,6 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+
+WAGTAILIMAGES_IMAGE_MODEL = "images.CustomImage"
