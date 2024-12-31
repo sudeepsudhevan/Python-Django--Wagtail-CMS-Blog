9ee52865e7da7af188ad4ea5e9e7523a8254217b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-12-31 15:32:44 +0530

    understand wagtail page context

--- blogpages/models.py
+++ blogpages/models.py
@@ -3,11 +3,12 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.images import get_image_model
 
 class BlogIndex(Page):
     # A listing page for blog entries(child pages)
 
-    # max_count = 1
+    max_count = 1
     parent_page_types = ['home.HomePage']
     subpage_types = ['blogpages.BlogDetail']
 
@@ -19,6 +20,11 @@ class BlogIndex(Page):
         FieldPanel('body'),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['blogpages'] = BlogDetail.objects.live().public()
+        return context
+
 
 class BlogDetail(Page):
     # A blog entry page
@@ -29,7 +35,16 @@ class BlogDetail(Page):
     subtitle = models.CharField(max_length=100, blank=True)
     body = RichTextField(blank=True)
 
+    image = models.ForeignKey(
+        get_image_model(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('body'),
+        FieldPanel('image'),
     ]
